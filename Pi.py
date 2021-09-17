a00e7c9c96f3a238f886893392d0f57498ab50d6
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-17 20:18:29 +0200

    Updated Pi.py

--- Pi.py
+++ Pi.py
@@ -6,6 +6,6 @@ class piCalculate(decimals) :
     
     for self.calculations < self.decimals :
         
-        self.pi *= ()
+        self.pi *= 1 ^ self.calculations
         
     print("The number is : " +self.pi)
