e78170f5fb4fd90c96f9cfd338fcda59ff3eabf9
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-17 20:14:59 +0200

    Created pi.py

---
+++ Pi.py
@@ -0,0 +1,11 @@
+class piCalculate(decimals) :
+    
+    self.calculations = 0
+    self.decimals = decimals
+    self.pi = 0
+    
+    for self.calculations < self.decimals :
+        
+        self.pi = ()
+        
+    print("The number is : " +self.pi)
