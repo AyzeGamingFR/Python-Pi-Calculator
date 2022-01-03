87b7f0ffb40ac1f721dfc39d6e412408c385828b
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-01-03 13:42:22 +0100

    Updated Pi.py

--- Pi.py
+++ Pi.py
@@ -1,11 +1,23 @@
 class piCalculate(decimals) :
     
     self.calculations = 0
+    self.character = "rem"
     self.decimals = decimals
-    self.pi = 0
+    self.number = 3
+    self.pi = 1
     
     for self.calculations < self.decimals :
         
-        self.pi *= 1 ^ self.calculations
-        
+        if self.character == "rem" :
+            
+            self.pi -= 1 / self.number
+            self.character = "add"
+            self.number += 2
+            
+        if self.character == "add" :
+            
+            self.pi += 1 / self.number
+            self.character = "rem"
+            self.number += 2
+            
     print("The number is : " +self.pi)
