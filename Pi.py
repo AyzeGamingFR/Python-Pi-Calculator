24e1eed59e03dbd98230504f944bb66b1895cc59
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-03-01 15:18:06 +0100

    Updated Pi.py

--- Pi.py
+++ Pi.py
@@ -1,23 +1,5 @@
-class piCalculate(self, decimals) :
+import Maths
+
+class piCalculate(self, x) :
     
-    self.calculations = 0
-    self.character = "rem"
-    self.decimals = decimals
-    self.number = 3
-    self.pi = 1
-    
-    for self.calculations < self.decimals :
-        
-        if self.character == "rem" :
-            
-            self.pi -= 1 / self.number
-            self.character = "add"
-            self.number += 2
-            
-        if self.character == "add" :
-            
-            self.pi += 1 / self.number
-            self.character = "rem"
-            self.number += 2
-            
-    print("The number is : " +(self.pi + 3))
+    print(x * Maths.sin(180/x))
