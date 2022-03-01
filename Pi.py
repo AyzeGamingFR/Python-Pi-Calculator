8184282ddb6072fb80d39506e44013f545edcd90
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-03-01 15:34:42 +0100

    Updated pi.py

--- Pi.py
+++ Pi.py
@@ -1,5 +1,25 @@
 import Maths
 
-class piCalculate(self, x) :
+class calculate_pi_sin_formula(self, x) :
     
-    print(x * Maths.sin(180/x))
+    print(x * Maths.sin(180 / x))
+
+class calculate_pi_nilakantha_formula(self, repetitions) :
+    
+    self.number = 2
+    self.pi = 3
+    for (repetitions < 0) :
+        
+        repetitions -= 1
+        if (self.nt === 1) :
+            
+            self.pi += (4 / (number * (number + 1) * ( number + 2))
+            self.number += 2
+            self.nt = 2
+            
+        else if (self.nt === 2) :
+            
+            self.pi -= (4 / (number * (number + 1) * (number + 2))
+            self.number += 2
+            self.nt = 1
+    print (self.pi)
